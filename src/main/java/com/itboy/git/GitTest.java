71edc1c4cbd350c3906bebb4b4629130e1d87d4d
root <root@qq.com>
2022-08-14 09:24:49 +0800

    hot-fix commit1

--- src/main/java/com/itboy/git/GitTest.java
+++ src/main/java/com/itboy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello summer!");
         System.out.println("hello everyone~");
+        System.out.println("hello programmer");
     }
 }
